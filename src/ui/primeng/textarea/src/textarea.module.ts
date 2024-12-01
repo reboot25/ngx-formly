dc211131ae6913baded9660b64d060ad823c98e7
Jun Yuan <yuanjuns@gmail.com>
2024-12-01 11:17:55 +0800

    feat(primeng): use primeng v18

--- src/ui/primeng/textarea/src/textarea.module.ts
+++ src/ui/primeng/textarea/src/textarea.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormlyModule } from '@ngx-formly/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { InputTextareaModule } from 'primeng/inputtextarea';
+import { Textarea } from 'primeng/inputtextarea';
 import { FormlyFormFieldModule } from '@ngx-formly/primeng/form-field';
 import { FormlyFieldTextArea } from './textarea.type';
 import { withFormlyFieldTextArea } from './textarea.config';
@@ -12,7 +12,7 @@ import { withFormlyFieldTextArea } from './textarea.config';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    InputTextareaModule,
+    Textarea,
 
     FormlyFormFieldModule,
     FormlyModule.forChild(withFormlyFieldTextArea()),
